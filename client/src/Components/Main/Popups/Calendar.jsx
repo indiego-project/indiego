592f1cabf784d9f9c06798df401b41eb4c36ebb8
Yeon <yeonhwan619@gmail.com>
2023-04-02 19:31:39 +0900

    #7 메인페이지 팝업 캘린더 width 수정

--- client/src/Components/Main/Popups/Calendar.jsx
+++ client/src/Components/Main/Popups/Calendar.jsx
@@ -21,7 +21,7 @@ import Spinner from "../../Spinner";
 
 const Container = styled.div`
   width: 100%;
-  max-width: 350px;
+  max-width: 400px;
   height: 80%;
   display: flex;
   justify-content: center;
