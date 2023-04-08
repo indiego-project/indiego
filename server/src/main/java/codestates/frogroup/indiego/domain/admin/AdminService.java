d3ead0a4d80c2901ce84f415c3f40d5c213a9e1d
kimjoenghui <20190896@sungshin.ac.kr>
2023-04-08 18:27:12 +0900

    #39 어드민 서비스 클래스 생성

---
+++ server/src/main/java/codestates/frogroup/indiego/domain/admin/AdminService.java
@@ -0,0 +1,7 @@
+//package codestates.frogroup.indiego.domain.admin;
+//
+//import codestates.frogroup.indiego.domain.member.entity.Member;
+//
+//public interface AdminService {
+//    CertificationDto.Response addPerformer(Member member );
+//}
