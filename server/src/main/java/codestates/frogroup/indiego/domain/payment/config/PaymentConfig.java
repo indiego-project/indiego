3baa7c3fa3de00c1e00750932f6194c226868fe9
Sungho95 <tjdgh1785@gmail.com>
2023-04-09 23:05:11 +0900

    #25 - PaymentConfig 수정
    - @ConfigurationProperties 사용(실패)
    - 스프링 빈으로 등록되지 않는 문제 발생, 해결 후 사용해야 함
    - build.gradle : ConfigurationProperties 사용을 위한 configuration-processor 의존성 추가

--- server/src/main/java/codestates/frogroup/indiego/domain/payment/config/PaymentConfig.java
+++ server/src/main/java/codestates/frogroup/indiego/domain/payment/config/PaymentConfig.java
@@ -1,8 +1,11 @@
 package codestates.frogroup.indiego.domain.payment.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+//@ConfigurationProperties(prefix = "payments") // 스프링 빈으로 등록되지 않는 문제 발생
 @Configuration
 public class PaymentConfig {
 
