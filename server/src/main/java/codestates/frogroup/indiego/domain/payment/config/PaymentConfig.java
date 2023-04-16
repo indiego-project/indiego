00f333332aa050e98ccf179efc422f53ed8cfec0
Sungho95 <tjdgh1785@gmail.com>
2023-04-16 20:46:20 +0900

    #25 - Payment 수정, PaymentConfig 수정, PaymentFailDto 작성
    - Payment 엔티티 : 실패시 사유, paymentKey 필드 추가
    - PaymentConfig : 스프링 빈 등록 문제로 다시 수정(시몬님꼐 질문하기)
    - PaymentFailDto : 결제 실패 시 응답 dto

--- server/src/main/java/codestates/frogroup/indiego/domain/payment/config/PaymentConfig.java
+++ server/src/main/java/codestates/frogroup/indiego/domain/payment/config/PaymentConfig.java
@@ -1,13 +1,17 @@
 package codestates.frogroup.indiego.domain.payment.config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 //@ConfigurationProperties(prefix = "payments") // 스프링 빈으로 등록되지 않는 문제 발생
+@Getter
 @Configuration
 public class PaymentConfig {
+
+    public static final String URL = "https://api.tosspayments.com/v1/payments/";
 
     @Value("${payments.toss.test_client_key}")
     private String testClientKey;
