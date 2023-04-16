099261fe472fa9877eae151c0d456de4db8eb2dc
Sungho95 <tjdgh1785@gmail.com>
2023-04-16 20:44:03 +0900

    #25 - PaymentRepository 결제 Repository 작성
    - findByOrderId : 주문 id를 통해 Payment 객체 조회

---
+++ server/src/main/java/codestates/frogroup/indiego/domain/payment/repository/PaymentRepository.java
@@ -0,0 +1,12 @@
+package codestates.frogroup.indiego.domain.payment.repository;
+
+import codestates.frogroup.indiego.domain.payment.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findByOrderId(String orderId);
+}
