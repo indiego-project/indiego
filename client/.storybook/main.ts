73b83d0c94cc23e1a82c61cb5624fd4bd03f31fd
ChoiYongWon <yongwon0824@naver.com>
2023-10-10 22:17:35 +0900

    bug: #267 storybook 오류 수정 1. svg 인식 오류 (webpack 추가) 2. tailwind 적용 x 오류

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -11,8 +11,32 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-styling-webpack"
+    "@storybook/addon-styling-webpack",
   ],
+  webpackFinal: async (config) => {
+    if (!config.module || !config.module.rules) {
+      return config;
+    }
+
+    config.module.rules = [
+      ...config.module.rules.map((rule) => {
+        if (!rule || rule === "...") {
+          return rule;
+        }
+
+        if (rule.test && /svg/.test(String(rule.test))) {
+          return { ...rule, exclude: /\.svg$/i };
+        }
+        return rule;
+      }),
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+    ];
+
+    return config;
+  },
   framework: {
     name: "@storybook/nextjs",
     options: {},
