0e3bf91b143ddae2cef100b3ab18fca49e64266e
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-10-22 15:09:40 +0900

    #270 - 스프링 시큐리티 초기 설정

--- serverkt/build.gradle.kts
+++ serverkt/build.gradle.kts
@@ -25,8 +25,19 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-//	runtimeOnly("com.mysql:mysql-connector-j")
+
+	// security, jwt, oauth2
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.mysql:mysql-connector-j")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 }
 
 allOpen {
